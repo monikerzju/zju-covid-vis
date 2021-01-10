60bf227873f657ca53b33388e5ad9bf6162499b6
CCMIKK <60166792+CCMIKK@users.noreply.github.com>
2021-01-10 15:15:23 +0800

    update code

--- src/python/cabarDataPreprocess.py
+++ src/python/cabarDataPreprocess.py
@@ -3,28 +3,37 @@ import json
 import pandas as pd
 f = open('../assets/json/countries_data.json')
 data = json.load(f)
+dataAll = []
 
-listHead = []
-listHead.append('国家或地区')
-for i in range(len(data[0]['list'])):
-  listHead.append(data[0]['list'][i]['date'])
+def getData(dataType):
+    listHead = []
+    listHead.append('国家或地区')
+    for i in range(len(data[0]['list'])):
+      listHead.append(data[0]['list'][i]['date'])
 
-dateList = [[] for i in range(len(data)+1)]
-dateList[0] = listHead
-for i in range(len(data)):
-  dateList[i+1].append(data[i]['country'])
-  for k in range(len(listHead)-1):
-    dateList[i+1].append(str(data[i]['list'][k]['total_confirm']))
+    dateList = [[] for i in range(len(data)+1)]
+    dateList[0] = listHead
+    for i in range(len(data)):
+      dateList[i+1].append(data[i]['country'])
+      for k in range(len(listHead)-1):
+        dateList[i+1].append(str(data[i]['list'][k][dataType]))
 
-listP = pd.DataFrame(dateList)
+    return dateList
 
-strList = list(str(dateList))
+dataAll.append(getData("total_confirm"))
+dataAll.append(getData("total_death"))
+dataAll.append(getData("total_cure"))
+
+strList = list(str(dataAll))
 for i in range(len(strList)-2):
   if ((strList[i] == '\'') & ((strList[i+1] != 'I') | (strList[i+2] != 'v'))):
-    strList[i] = '\"'
+      strList[i] = '\"'
 strList = ''.join(strList)
 print(strList)
+
 
 file = open('../assets/json/cabarData.json','w',encoding='utf-8')
 file.write(strList)
 file.close()
+
+
